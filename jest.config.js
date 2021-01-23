33db7e9a0e6c75bce7d3173edfd21e912e6fa525
xantabdoc <67368298+xantabdoc@users.noreply.github.com>
2021-01-23 16:11:44 +0700

    Configuring Module

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+const {defaults} = require('jest-config');
+module.exports = {
+    "verbose": true,
+    moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx', 'js'],
+    "transform": {
+        "^.+\\.jsx?$": "babel-jest"
+    },
+    "transformIgnorePatterns": [
+        "node_modules/(?!(@babel)/)"
+    ],
+}
