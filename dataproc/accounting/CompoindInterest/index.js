9b7f87c7cde589b31cf6674d67c85692e7d6f4ae
xantabdoc <67368298+xantabdoc@users.noreply.github.com>
2021-01-23 16:56:51 +0700

    add Feature and Tester for Compound Interest

--- dataproc/accounting/CompoindInterest/index.js
+++ dataproc/accounting/CompoindInterest/index.js
@@ -4,7 +4,7 @@ const presentByFuture = (i, n) => (Math.pow(1 + i, -n));
 const annualByFuture = (i, n) => (i / (Math.pow(1 + i, n) - 1));
 const annualByPresent = (i, n) => (i * Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1);
 const futureByAnnual = (i, n) => ((Math.pow(1 + i, n) - 1) / i);
-const presentByAnnual = (i, n) => (Math.pow((1 + i), n) - 1) / (i * Math.pow((1+i), n));
+const presentByAnnual = (i, n) => (Math.pow((1 + i), n) - 1) / (i * Math.pow((1 + i), n));
 const annualByGradient = (i, n) => (1 / i - n / (Math.pow(1 + i, n) - 1));
 const presentByGradient = (i, n) => (Math.pow(1 + i, n) - i * n - 1) / (Math.pow(i, 2) * Math.pow(1 + i, n));
 
@@ -24,17 +24,45 @@ class CompoundInterest {
         return this;
     }
 
+    annualByFuture(i, n) {
+        this.ratio = annualByFuture(i, n);
+        return this;
+    }
+
+    annualByPresent(i, n) {
+        this.ratio = annualByPresent(i, n);
+        return this;
+    }
+
+    futureByAnnual(i, n) {
+        this.ratio = futureByAnnual(i, n);
+        return this;
+    }
+
+    presentByAnnual(i, n) {
+        this.ratio = presentByAnnual(i, n);
+        return this;
+    }
+
+    annualByGradient(i, n) {
+        this.ratio = annualByGradient(i, n);
+        return this;
+    }
+
+    presentByGradient(i, n) {
+        this.ratio = presentByGradient(i, n);
+        return this;
+    }
+
     getMoney(money) {
         return this.ratio * money;
     }
-    getRatio(){
+
+    getRatio() {
         return this.ratio;
     }
 }
 
-// function getcover({func, i, n}) {
-//     return funclist[func()]
-// }
 export default CompoundInterest;
 export {
     futureByPresent,
--- dataproc/accounting/CompoindInterest/test.spec.js
+++ dataproc/accounting/CompoindInterest/test.spec.js
@@ -56,10 +56,74 @@ describe('Compound Interest Function Testing', () => {
             expect(presentByGradient(0.01, 2)).toBeWithinRange(0.98, 0.99);
         });
     })
-    describe('Classes Function Testing',()=>{
+    describe('Classes Function Testing', () => {
         test('Classes Function Testing', () => {
             const feature = new CompoundInterest();
             expect(feature.presentByFuture(0.01, 2).getRatio()).toBeWithinRange(0.98, 0.99);
+        });
+        test('Get Future Ratio By Present Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.futureByPresent(0.01, 2).getRatio()).toBeWithinRange(1, 1.1);
+        });
+        test('Get Future Money By Present Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.futureByPresent(0.01, 2).getMoney(100000)).toBeWithinRange(100000, 110000);
+        });
+        test('Get Present Ratio By Future Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.presentByFuture(0.01, 2).getRatio()).toBeWithinRange(0.98, 0.99);
+        });
+        test('Get Present Money By Future Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.presentByFuture(0.01, 2).getMoney(100000)).toBeWithinRange(98000, 99000);
+        });
+        test('Get Annual Ratio By Future Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.annualByFuture(0.01, 2).getRatio()).toBeWithinRange(0.4, 0.5);
+        });
+        test('Get Annual Money By Future Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.annualByFuture(0.01, 2).getMoney(100000)).toBeWithinRange(40000, 50000);
+        });
+        test('Get Annual Ratio By Present Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.annualByPresent(0.01, 2).getRatio()).toBeWithinRange(0.5, 0.6);
+        });
+        test('Get Annual Money By Present Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.annualByPresent(0.01, 2).getMoney(100000)).toBeWithinRange(50000, 60000);
+        });
+        test('Get Future Ratio By Annual Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.futureByAnnual(0.01, 2).getRatio()).toBeWithinRange(2, 2.1);
+        });
+        test('Get Future Money By Annual Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.futureByAnnual(0.01, 2).getMoney(100000)).toBeWithinRange(200000, 210000);
+        });
+        test('Get Present Ratio By Annual Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.presentByAnnual(0.01, 2).getRatio()).toBeWithinRange(1.9, 2);
+        });
+        test('Get Present Money By Annual Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.presentByAnnual(0.01, 2).getMoney(100000)).toBeWithinRange(190000, 200000);
+        });
+        test('Get Annual Ratio By Gradient Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.annualByGradient(0.01, 2).getRatio()).toBeWithinRange(0.4, 0.5);
+        });
+        test('Get Annual Money By Gradient Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.annualByGradient(0.01, 2).getMoney(100000)).toBeWithinRange(40000, 50000);
+        });
+        test('Get Present Ratio By Gradient Ratio', () => {
+            const feature = new CompoundInterest();
+            expect(feature.presentByGradient(0.01, 2).getRatio()).toBeWithinRange(0.98, 0.99);
+        });
+        test('Get Present Money By Gradient Money', () => {
+            const feature = new CompoundInterest();
+            expect(feature.presentByGradient(0.01, 2).getMoney(100000)).toBeWithinRange(98000, 99000);
         });
     });
 
